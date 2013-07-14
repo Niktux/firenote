898e4000b978622bfcaf9717f262d429007cd54d
Niktux <nico.ln@gmail.com>
2013-07-14 03:12:24 +0200

    FIX autoloading issue

--- src/Firenote/Console.php
+++ src/Firenote/Console.php
@@ -4,7 +4,7 @@ namespace Firenote;
 
 use Symfony\Component\Console\Application;
 
-require __DIR__ . '/../bootstrap.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
 $app = new Application();
 $app->add(new Command\ApplicationInit());
